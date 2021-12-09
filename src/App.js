8f4f51df38c53385cb5494c89b594f3be9bd6481
Prateek Verma <prateekverma70@gmail.com>
2021-12-10 03:01:55 +0530

    feat:numbering to adjacent mines added to tiles

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import "./App.css";
 import MatrixBoard from "./Components/MatrixBoard";
 
 function App() {
+  const [row] = useState(8);
+  const [column] = useState(8);
   const [matrix, setMatrix] = useState(() => {
     let tempMatrix = [];
-    for (let i = 0; i < 8; i++) {
+    for (let i = 0; i < column; i++) {
       tempMatrix.push([0, 0, 0, 0, 0, 0, 0, 0]);
     }
     return tempMatrix;
@@ -25,8 +27,38 @@ function App() {
     setMatrix([...tempMatrix]);
   };
 
+  const handleAdjacentMineNumber = () => {
+    let tempMatrix = matrix;
+
+    for (let i = 0; i < row; i++) {
+      for (let j = 0; j < column; j++) {
+        let adjacentMineCount = 0;
+        //0 2
+        if (tempMatrix[i][j] === "💣") continue;
+
+        if (i - 1 >= 0 && j - 1 >= 0 && tempMatrix[i - 1][j - 1] === "💣")
+          adjacentMineCount++;
+        if (i - 1 >= 0 && tempMatrix[i - 1][j] === "💣") adjacentMineCount++;
+        if (i - 1 >= 0 && j + 1 < column && tempMatrix[i - 1][j + 1] === "💣")
+          adjacentMineCount++;
+        if (j - 1 >= 0 && tempMatrix[i][j - 1] === "💣") adjacentMineCount++;
+        if (i - 1 >= 0 && j + 1 < column && tempMatrix[i][j + 1] === "💣")
+          adjacentMineCount++;
+        if (i + 1 < row && j - 1 >= 0 && tempMatrix[i + 1][j - 1] === "💣")
+          adjacentMineCount++;
+        if (i + 1 < row && tempMatrix[i + 1][j] === "💣") adjacentMineCount++;
+        if (i + 1 < row && j + 1 < column && tempMatrix[i + 1][j + 1] === "💣")
+          adjacentMineCount++;
+
+        tempMatrix[i][j] = adjacentMineCount;
+      }
+    }
+    setMatrix([...tempMatrix]);
+  };
+
   useEffect(() => {
     handleMatrixMines();
+    handleAdjacentMineNumber();
   }, []);
 
   return (
